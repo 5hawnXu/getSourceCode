f41c893186e51a9717e4ce6c338272078e5f0c42
5hawnXu <shawn@hxzy.me>
2024-03-31 16:04:34 +0700

    fix bugs

--- getSourceCode/common.py
+++ getSourceCode/common.py
@@ -4,7 +4,7 @@ import time
 
 # Package base info
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-current_version = "2.0.7"
+current_version = "2.0.8"
 name = "getSourceCode"
 proxy_contract = {}
 contract_index = 0
